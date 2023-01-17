4110d56d4ca5e9cdac1e6d73f3189a1f2c4bf09f
ioncrisa2 <zyohanes99@gmail.com>
2023-01-17 07:17:11 +0700

    change pagination default to 20

--- src/views/company/index.vue
+++ src/views/company/index.vue
@@ -169,7 +169,7 @@ onMounted(async () => {
       companies.value = response;
       setTimeout(() => {
         $("#table").DataTable({
-          lengthMenu: [5, 10, 20, 50, 100, 200, 500],
+          lengthMenu: [20, 50, 100, 200, 500],
           language: { emptyTable: "Data belum tersedia!!" }
         });
       })
